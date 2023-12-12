2178d5cae2471da5196fd8b843418e7db55c64be
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-12-12 09:01:58 +0100

    update

---
+++ src/main/java/ma/enset/KafkaSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package ma.enset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringCloudApplication.class, args);
+    }
+
+}
